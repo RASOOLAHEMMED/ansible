2cdcbd788cd3eda69201e882476f435fe9c14278
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-08-05 12:07:19 +0530

    for creating instances using terraform

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,30 +1,20 @@
-resource "aws_spot_instance_request" "cheap_worker" {
-  count                     = local.LENGTH
-  ami                       = "ami-074df373d6bafa625"
-  spot_price                = "0.0031"
-  instance_type             = "t3.micro"
-  vpc_security_group_ids    = ["sg-0db10ae5ed60c9988"]
-  wait_for_fulfillment      = true
-  //spot_type                 = "persistent"
-  tags                      = {
-    Name                    = element(var.COMPONENTS, count.index)
+resource "aws_instance" "sample" {
+  count                       = local.LENGTH
+  ami                         = "ami-074df373d6bafa625"
+  instance_type               = "t3.micro"
+  vpc_security_group_ids      = ["sg-0db10ae5ed60c9988"]
+  tags                        = {
+    Name                      = element(var.COMPONENTS, count.index )
   }
 }
 
-resource "aws_ec2_tag" "name-tag" {
-  count                     = local.LENGTH
-  resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-  key                       = "Name"
-  value                     = element(var.COMPONENTS, count.index)
-}
-
 resource "aws_route53_record" "records" {
-  count                     = local.LENGTH
-  name                      = element(var.COMPONENTS, count.index)
-  type                      = "A"
-  zone_id                   = "Z00947631JDBITKOLUVH1"
-  ttl                       = 300
-  records                   = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
+  count                         = local.LENGTH
+  name                          = element(var.COMPONENTS, count.index )
+  type                          = "A"
+  zone_id                       = "Z00947631JDBITKOLUVH1"
+  ttl                           = 300
+  records                       = [element(aws_instance.sample.*.private_ip, count.index )]
 }
 
 
